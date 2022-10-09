bf266c989dd6ab8db8db598cc3420d66c8f98035
Joel Sereno <jakayus123@gmail.com>
2022-10-08 19:03:37 -0700

    Add WatchApp target to existing project
    Add WatchApp target so that a watch app can be created. Watch displays default Hello World.

---
+++ brain-marks-watch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  brain-marks-watch Watch App
+//
+//  Created by Joel Sereno on 10/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ brain-marks-watch Watch App/brain_marks_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  brain_marks_watchApp.swift
+//  brain-marks-watch Watch App
+//
+//  Created by Joel Sereno on 10/8/22.
+//
+
+import SwiftUI
+
+@main
+struct brain_marks_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
